657b3ebb34848d1f931394c1c2bc29eed8e9078b
insuhkim <insuhkim@naver.com>
2025-08-29 19:32:35 +0900

    Update git config and add aliases

--- home/git.nix
+++ home/git.nix
@@ -3,18 +3,39 @@
     enable = true;
     userName = "insuhkim";
     userEmail = "insuhkim@naver.com";
-    # delta.enable = true;
+    delta.enable = true;
     # difftastic.enable = true;
-    diff-so-fancy.enable = true;
+    # diff-so-fancy.enable = true;
     aliases = {
       co = "checkout";
       ci = "commit";
       st = "status -sb";
       br = "branch";
-      # hist = "log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short";
+      amend = "commit --amend";
+      # ac = "add -A && commit -m";
+      ac = "commit -am";
+      unstage = "reset HEAD --";
+      aliases = "!git config --get-regexp ^alias\\. | sed -e 's/^alias\\.//' -e 's/ / = /'";
+      pushforce = "push --force-with-lease";
+      branches = "branch -a";
       hist = "log --all --graph --pretty=format:'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
       type = "cat-file -t";
       dump = "cat-file -p";
+    };
+
+    extraConfig = {
+      init.defaultBranch = "main";
+      pull.rebase = true;
+      push.default = "current";
+
+      merge.conflictStyle = "zdiff3";
+      diff.algorithm = "histogram";
+      diff.colorMoved = "default";
+
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      push.autoSetupRemote = true;
+      rerere.enabled = true;
     };
   };
 }
