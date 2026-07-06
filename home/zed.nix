35f04fae811dd0b8b8c2f72e41999d5cdbd61914
insuhkim <insuhkim@naver.com>
2026-07-06 23:50:51 +0900

    feat: implement zed settings

--- home/zed.nix
+++ home/zed.nix
@@ -10,11 +10,61 @@ in
   config = lib.mkIf cfg.enable {
     programs.zed-editor = {
       enable = true;
-      extensions = [
-        "nix"
-        "catppuccin"
-        "catppuccin-icons"
-      ];
+      userSettings = {
+        terminal = {
+          font_size = 15;
+        };
+        buffer_line_height = "comfortable";
+        buffer_font_weight = 400;
+        buffer_font_size = 16;
+        icon_theme = {
+          mode = "system";
+          light = "Catppuccin Frappé";
+          dark = "Catppuccin Frappé";
+        };
+        auto_update = false;
+        which_key = {
+          delay_ms = 100;
+          enabled = true;
+        };
+        vim = {
+          use_smartcase_find = true;
+          use_system_clipboard = "never";
+          toggle_relative_line_numbers = true;
+        };
+        project_panel = {
+          dock = "left";
+        };
+        outline_panel = {
+          dock = "left";
+        };
+        collaboration_panel = {
+          dock = "left";
+        };
+        git_panel = {
+          dock = "left";
+        };
+        ui_font_size = 16;
+        theme = {
+          mode = "system";
+          light = "Catppuccin Latte";
+          dark = "Catppuccin Frappé";
+        };
+        vim_mode = true;
+        languages = {
+          Nix = {
+            language_servers = [
+              "nixd"
+              "!nil"
+            ];
+          };
+        };
+        auto_install_extensions = {
+          catppuccin = true;
+          catppuccin-icons = true;
+          nix = true;
+        };
+      };
     };
   };
 }
