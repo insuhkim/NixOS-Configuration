ed0f025386f34ec3966d28079ed7257367c75d10
insuhkim <insuhkim@naver.com>
2026-09-25 09:18:19 +0900

    refactor: remove nested dir

--- home/dotfiles/default.nix
+++ home/dotfiles/default.nix
@@ -2,7 +2,7 @@
   xdg.configFile."blesh/init.sh".source = ./.blerc;
   xdg.configFile."starship.toml".source = ./starship.toml;
   xdg.configFile."mango" = {
-    source = ./mango/config;
+    source = ./mango;
     recursive = true;
   };
 }
